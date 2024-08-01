12e825da64dbec27bfe680cfdb742ae7c06bd1cc
প্রদীপ দাস (Pradip Das) <97117591+pradipdas9040@users.noreply.github.com>
2024-08-01 09:15:48 +0530

    Add files via upload

---
+++ dateDict.py
@@ -0,0 +1,34 @@
+def return_info():
+    date_dict = {
+        '10-02' : {
+            'name' : 'Dhananjay',
+            'email' : 'dhananjaymath7@gmail.com',
+            'emogi' : '🥳'
+        },
+        '27-07' : {
+            'name' : 'Dipti',
+            'email' : 'photorphoto1@gmail.com',
+            'emogi' : '🍻'
+        },
+        '29-05' : {
+            'name' : 'Kuntal',
+            'email' : 'kuntalpramanick55@gmail.com',
+            'emogi' : '🍻'
+        },
+        '28-07' : {
+            'name' : 'Anupam',
+            'email' : 'mondalanupam4g@gmail.com',
+            'emogi' : '🍻'
+        },
+        '17-09' : {
+            'name' : 'Piyali',
+            'email' : 'piyalipaulmail@gmail.com',
+            'emogi' : '🥳'
+        },
+        '01-08' : {
+            'name' : 'ghh',
+            'email' : 'photorphoto1@gmail.com',
+            'emogi' : '🥳'
+        }
+    }
+    return date_dict
